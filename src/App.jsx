35a4125a1462dd5325d8454c7856d9ad2f0baf42
meghraj thakre <meghrajthakre444@gmail.com>
2025-11-29 18:12:52 +0530

    Initial commit to the project

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import  Navbar  from '../src/components/Navbar';
+import Footer from './components/Footer';
+const App = () => {
+  return (
+    <div>
+      <Navbar/>
+      <Footer/>
+    </div>
+  )
+}
+
+export default App
