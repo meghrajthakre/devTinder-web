007e92235bc0565b00da6f5387f91774bc203368
meghraj thakre <meghrajthakre444@gmail.com>
2026-01-13 18:40:47 +0530

    Refactor code structure for improved readability and maintainability

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -59,7 +59,7 @@ const Navbar = () => {
       {/* LEFT */}
       <div className="flex items-center gap-3">
         <img
-          src="src/assets/Logo/logo.png"
+          src="public/assets/logo.png"
           className="w-10 h-10"
           alt="DevTinder Logo"
         />
