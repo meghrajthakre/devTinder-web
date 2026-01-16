6b667576b2b583d041690431a74a0d4df7c597cd
meghraj thakre <meghrajthakre444@gmail.com>
2026-01-16 14:55:28 +0530

    fix: remove server proxy configuration from Vite config

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,6 @@ export default defineConfig({
   plugins: [
     tailwindcss()
   ],
-  server: {
-    proxy: {
-      "/api": {
-        target: "http://localhost:3001",
-        changeOrigin: true,
-      },
-    },
-  },
+ 
 
 });
